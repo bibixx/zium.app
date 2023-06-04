30a170d4f953fb7dde51307aefd1f499d9692461
Bartosz Legięć <bartosz@legiec.io>
2023-06-04 19:52:34 +0200

    Make hover effect adjustments

--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { withAs } from "../../utils/withAs";
 import { CountryImage } from "../CountryImage/CountryImage";
+import { WithVariables } from "../WithVariables/WithVariables";
 import styles from "./EventCard.module.scss";
 
 interface EventCardProps {
@@ -12,30 +14,59 @@ interface EventCardProps {
 }
 export const EventCard = withAs("button")<EventCardProps>(
   ({ as: Component, pictureUrl, countryName, countryId, displayDate, caption, description, ...props }, ref) => {
+    const [cursorPosition, setCursorPosition] = useState<{ x: number | undefined; y: number | undefined }>({
+      x: undefined,
+      y: undefined,
+    });
+
     return (
-      <Component className={styles.wrapper} {...props} ref={ref}>
-        <div className={styles.heroImageWrapper}>
-          <img
-            src={`https://f1tv.formula1.com/image-resizer/image/${pictureUrl}?w=400&h=195&q=HI&o=L`}
-            alt=""
-            className={styles.heroImage}
-          />
-          <div className={styles.heroImageOverlay} />
-        </div>
-        <main className={styles.contentWrapper}>
-          <div className={styles.header}>
-            {countryId && <CountryImage countryId={countryId} width={40} height={40} />}
-            <div className={styles.headerTextContent}>
-              <div className={styles.headerFirstLine}>{countryName}</div>
-              <div className={styles.headerSecondLine}>
-                <div>{displayDate}</div>
-                {caption && <div className={styles.headerRound}>{caption}</div>}
+      <WithVariables
+        className={styles.hoverPositionWrapper}
+        variables={{
+          "x-pos": cursorPosition.x,
+          "y-pos": cursorPosition.y,
+        }}
+        onMouseMove={(e) => {
+          const boundingBox = e.currentTarget.getBoundingClientRect();
+          const leftX = e.pageX - (boundingBox.left + window.scrollX);
+          const topY = e.pageY - (boundingBox.top + window.scrollY);
+
+          setCursorPosition({
+            x: (leftX - boundingBox.width / 2) / (boundingBox.width / 2),
+            y: (topY - boundingBox.height / 2) / (boundingBox.height / 2),
+          });
+        }}
+        onMouseLeave={() => {
+          setCursorPosition({
+            x: undefined,
+            y: undefined,
+          });
+        }}
+      >
+        <Component className={styles.wrapper} {...props} ref={ref}>
+          <div className={styles.heroImageWrapper}>
+            <img
+              src={`https://f1tv.formula1.com/image-resizer/image/${pictureUrl}?w=400&h=195&q=HI&o=L`}
+              alt=""
+              className={styles.heroImage}
+              draggable={false}
+            />
+          </div>
+          <main className={styles.contentWrapper}>
+            <div className={styles.header}>
+              {countryId && <CountryImage countryId={countryId} width={40} height={40} />}
+              <div className={styles.headerTextContent}>
+                <div className={styles.headerFirstLine}>{countryName}</div>
+                <div className={styles.headerSecondLine}>
+                  <div>{displayDate}</div>
+                  {caption && <div className={styles.headerRound}>{caption}</div>}
+                </div>
               </div>
             </div>
-          </div>
-          {description && <div className={styles.content}>{description}</div>}
-        </main>
-      </Component>
+            {description && <div className={styles.content}>{description}</div>}
+          </main>
+        </Component>
+      </WithVariables>
     );
   },
 );
--- src/components/WithVariables/WithVariables.tsx
+++ src/components/WithVariables/WithVariables.tsx
@@ -1,6 +1,6 @@
 import React, { forwardRef } from "react";
 
-type Variables = Record<string, string | number>;
+type Variables = Record<string, string | number | undefined>;
 type BaseProps = JSX.IntrinsicElements["div"];
 export interface WithVariableProps extends BaseProps {
   as?: React.ElementType | undefined;
@@ -14,7 +14,11 @@ export const WithVariables = forwardRef<HTMLDivElement, WithVariableProps>(
 );
 
 export const getStylesWithVariables = (variables: Variables, style: React.CSSProperties = {}) => {
-  const variablesAsStyles = Object.fromEntries(Object.entries(variables).map(([key, value]) => [`--${key}`, value]));
+  const variablesAsStyles = Object.fromEntries(
+    Object.entries(variables)
+      .filter(([, value]) => value != null)
+      .map(([key, value]) => [`--${key}`, value]),
+  );
 
   return {
     ...style,
