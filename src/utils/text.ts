1d806de34c98407aab844f7ace6bb3951a92e5eb
Bartosz Legięć <bartosz@legiec.io>
2023-06-17 14:29:51 +0200

    Extract non-breaking hyphen

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -18,5 +18,6 @@ export const NBSP = "\xa0";
 export const MDASH = "\u2014";
 export const OPEN_QUOTE = "\u201c";
 export const CLOSE_QUOTE = "\u201d";
+export const NON_BREAKING_HYPHEN = "\u2011";
 
 export const quote = (text: string) => `${OPEN_QUOTE}${text}${CLOSE_QUOTE}`;
