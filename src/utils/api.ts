3a7365b2430eb6ca0b071fc5615288dafaf4dd0b
Bartosz Legięć <bartosz@legiec.io>
2023-09-15 10:18:26 +0200

    Change referrer

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,2 +1,6 @@
+const defaultOptions: Partial<RequestInit> = {
+  referrer: "",
+};
+
 export const fetchJSON = (url: string, init: RequestInit | undefined, signal: AbortSignal): Promise<unknown> =>
-  fetch(`https://f1tv.formula1.com${url}`, { ...init, signal }).then((res) => res.json());
+  fetch(`https://f1tv.formula1.com${url}`, { ...defaultOptions, ...init, signal }).then((res) => res.json());
