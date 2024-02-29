994577858230aea04e9cf9a8ffa2f848c8a1fdf9
Bartosz Legięć <bartosz@legiec.io>
2024-02-29 14:17:38 +0100

    Use text constant instead of text literal

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,8 +1,9 @@
 import { format } from "date-fns";
+import { NDASH } from "./text";
 
 export const formatDateDayShortMonth = (date: Date) => format(date, "d MMM");
 export const formatDateDayLongMonthYear = (date: Date) => format(date, "d MMMM yyyy");
 export const formatDateDayShortMonthRange = (startDate: Date, endDate: Date) =>
-  `${formatDateDayShortMonth(startDate)}–${formatDateDayShortMonth(endDate)}`;
+  `${formatDateDayShortMonth(startDate)}${NDASH}${formatDateDayShortMonth(endDate)}`;
 export const formatDateDayLongMonthYearTime = (date: Date) => format(date, "d MMMM yyyy 'at' HH:mm");
 export const formatDateDayShortMonthYear = (date: Date) => format(date, "MMM d, yyyy");
