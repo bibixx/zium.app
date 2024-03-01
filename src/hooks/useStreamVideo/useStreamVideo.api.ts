0646658c59a6d5f75e26377f8773ca0dc7b40f46
Bartosz Legięć <bartosz@legiec.io>
2024-03-01 17:45:10 +0100

    Fix zium (for now)

--- src/hooks/useStreamVideo/useStreamVideo.api.ts
+++ src/hooks/useStreamVideo/useStreamVideo.api.ts
@@ -13,7 +13,16 @@ export type VideoStreamInfo =
     };
 export const fetchVideoStream = async (playbackUrl: string, signal: AbortSignal): Promise<VideoStreamInfo> => {
   const baseUrl = "/2.0/R/ENG/WEB_HLS/ALL";
-  const body = await fetchJSON(`${baseUrl}/${playbackUrl}`, undefined, signal);
+  const body = await fetchJSON(
+    `${baseUrl}/${playbackUrl}`,
+    {
+      headers: {
+        "x-f1-device-info":
+          "device=web;screen=browser;os=mac os;browser=chrome;browserVersion=122.0.0.0;model=Macintosh;osVersion=14.3.1;appVersion=release-R29.0.3;playerVersion=8.129.0",
+      },
+    },
+    signal,
+  );
   const parsedBody = streamVideoBodyValidator.parse(body);
 
   if (parsedBody.resultObj.streamType === "HLS") {
